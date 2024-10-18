d418ea61c2152b5c2dcf6a5770888ba1928710fc
Amal Nair <amal.nair@subex.com>
2024-10-18 15:37:46 +0530

    Adding vi editor,cat,rmdir,echo

--- basher.js
+++ basher.js
@@ -22,6 +22,10 @@ const months = [
     "December",
 ];
 var workingDirectory = "/home/basher";
+//vim props
+var isInsertMode = false;
+var vimFileName = ""
+var vimStatus = ""
 
 function handleTextInput(event) {
     event.preventDefault();
@@ -33,10 +37,22 @@ function parseInput(stdin) {
     var processedOutputString = "basher@hello_world > " + stdin + "<br/>";
     var input = stdin.split(" ");
     switch (input[0]) {
+        case "":
+            break;
+
         case "clear":
             processedOutputString = "";
             clear();
             break;
+
+        case "cat":
+            processedOutputString += cat(input) + "<br/>";
+            break;
+
+        case "echo":
+            processedOutputString += echo(input) + "<br/>";
+            break;
+
 
         case "date":
             processedOutputString += date(input) + "<br/>";
@@ -60,16 +76,28 @@ function parseInput(stdin) {
             processedOutputString += pwd(input) + "<br/>";
             break;
 
+        case "vi":
+            startVim(input)
+            break;
+
+        case "rmdir":
+            rmdir(input)
+            processedOutputString += "<br/>";
+            break;
+
         default:
             processedOutputString +=
                 "Unrecognized command " + input[0] + "<br/>";
-            break;
     }
     document.getElementById("terminal").innerHTML += processedOutputString;
 }
 
 function clear() {
     document.getElementById("terminal").innerHTML = "";
+}
+
+function cat(input){
+    return window.localStorage.getItem(workingDirectory+"/"+input[1])
 }
 
 function date(input) {
@@ -103,7 +131,7 @@ function ls(input) {
     if (
         input.length > 1 &&
         input[1].startsWith("-") &&
-        input[1].includes("-")
+        input[1].includes("l")
     ) {
         longListFlag = true;
     }
@@ -166,14 +194,155 @@ function mkdir(input) {
     return "";
 }
 
+function echo(input) {
+    var outputString = ""
+    for (let i = 1; i < input.length; i++) {
+        outputString += input[i] + " "
+    }
+    outputString = outputString.replaceAll("\"", "")
+    return outputString
+}
+
 function cd(input) {
-    if (input[1].startsWith("/")) {
-        workingDirectory = input;
-    } else {
+    if (input[1] == "..") {
+        workingDirectory = workingDirectory.substring(0, workingDirectory.lastIndexOf("/"))
+    }
+    else if (input[1].startsWith("/")) {
+        workingDirectory = input[1];
+    } else if (input[1] != ".") {
         workingDirectory += "/" + input[1];
+    }
+}
+
+function rmdir(input) {
+    if (input.length >= 2) {
+        for (let j = 1; j < input.length; j++) {
+            if (input[j] != "." && input[j] != "..") {
+                if (input[j].startsWith("/")) {
+                    var files = window.localStorage.getItem(input[j])
+                    var folderContents = [];
+                    if (files) folderContents = JSON.parse("[" + files + "]");
+                    var updatedFolderContents = []
+                    for (let i = 0; i < folderContents.length; i++) {
+                        if (input[j] != folderContents[i].name) {
+                            updatedFolderContents.push(JSON.stringify(folderContents[i]))
+                        }
+                    }
+                    window.localStorage.setItem(input[j], updatedFolderContents)
+                } else {
+                    var files = window.localStorage.getItem(workingDirectory)
+                    var folderContents = [];
+                    if (files) folderContents = JSON.parse("[" + files + "]");
+                    var updatedFolderContents = []
+                    for (let i = 0; i < folderContents.length; i++) {
+                        if (input[j] != folderContents[i].name) {
+                            updatedFolderContents.push(JSON.stringify(folderContents[i]))
+                        }
+                    }
+                    window.localStorage.setItem(workingDirectory, updatedFolderContents)
+                }
+            }
+        }
     }
 }
 
 function pwd(input) {
     return workingDirectory;
+}
+
+function startVim(input) {
+    var root = document.getElementById("root")
+    var vim = document.getElementById("vim")
+    var viminput = document.getElementById("viminput")
+    var statusbar = document.getElementById("statusbar")
+
+    root.style.display = "none"
+    vim.style.display = "flex"
+    viminput.disabled = true
+    vimFileName = input[1].includes(".") ? input[1] : input[1] + ".txt"
+}
+
+function handleVimKeyDown(event) {
+    if (event.which == 13) {
+        closeVim()
+    }
+    switch (event.which) {
+        case 27:
+            disableInsertMode()
+            break;
+
+        default:
+            handleVimInput(event)
+    }
+    document.getElementById("statusbar").innerHTML = vimStatus
+}
+
+function handleVimInput(event) {
+    var which = event.which
+    switch (which) {
+        case 73:
+            if (isInsertMode && which != 16) {
+                document.getElementById("viminput").innerHTML = String.fromCharCode(which) + document.getElementById("viminput").innerHTML
+            }
+            else {
+                document.getElementById("viminput").disabled = false
+                vimStatus = "--INSERT--"
+                isInsertMode = true
+            }
+            break;
+        case 59:
+            if (!isInsertMode && event.shiftKey) {
+                vimStatus = ":"
+            }
+            break;
+        default:
+            if (isInsertMode && which != 16) document.getElementById("viminput").innerHTML = String.fromCharCode(which) + document.getElementById("viminput").innerHTML
+            else if (vimStatus != "") vimStatus += String.fromCharCode(which).toLocaleLowerCase()
+            break;
+    }
+}
+
+function disableInsertMode() {
+    document.getElementById("viminput").disabled = true
+    vimStatus = ""
+    isInsertMode = false
+}
+
+function closeVim() {
+    var root = document.getElementById("root")
+    var vim = document.getElementById("vim")
+    var viminput = document.getElementById("viminput")
+
+    root.style.display = "flex"
+    vim.style.display = "none"
+    var data = viminput.innerHTML
+    viminput.innerHTML = ""
+
+
+    var files = window.localStorage.getItem(workingDirectory)
+    var folderContents = [];
+    if (files) folderContents = JSON.parse("[" + files + "]");
+    const d = new Date();
+    const time = d.toLocaleTimeString([], {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: false,
+    });
+    var object = {
+        name: vimFileName,
+        modifiedAt:
+            d.getDate() +
+            " " +
+            months[d.getMonth()] +
+            " " +
+            d.getFullYear() +
+            " " +
+            time,
+        modifiedBy: "basher",
+        permissions: "-rw-rw-r--",
+    };
+    folderContents.push(JSON.stringify(object))
+    window.localStorage.setItem(workingDirectory, folderContents)
+    window.localStorage.setItem(workingDirectory + "/" + vimFileName, data)
 }
